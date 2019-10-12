5cd73780cf3b8ed1abea1aae5eb8838493c4a0ad
Waqas Khan <wuikhan@gmail.com>
2019-10-11 22:37:01 -0400

    j

--- iframe.php
+++ iframe.php
@@ -3,10 +3,9 @@
 
 <hr>
 <h1> Iframe Practice </h1>
-<input type="text" class="form-control" placeholder="Username" id="username" aria-label="Username" aria-describedby="basic-addon1">
-<br><br>
 
-<iframe id="frame1" src="login.html"></iframe>
-<iframe id="frame2" src="login.html"></iframe>
+
+<iframe id="frame1" src="login.php"></iframe>
+<iframe id="frame2" src="login.php"></iframe>
 
 <?php include('footer.php') ?>
