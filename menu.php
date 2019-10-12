6d037fe7fe09bbfe182fe41d526688e9e0005fbe
Waqas Khan <wuikhan@gmail.com>
2019-10-11 21:29:25 -0400

    test

---
+++ menu.php
@@ -0,0 +1,10 @@
+<div class="nav-scroller py-1 mb-2">
+      <nav class="nav d-flex justify-content-between">
+        <a class="p-2 text-muted alertLink" href="Home.html">Alerts</a>
+        <a class="p-2 text-muted" id="iframe" href="iframe.html">Iframes</a>
+        <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.html">Practice Multiple Windows</a>
+        <a class="p-2 text-muted" href="login.html">login</a>
+        <a class="p-2 text-muted" href="Table.html">Tables</a>
+        <a class="p-2 text-muted" href="bytag.html">By Tag</a>
+      </nav>
+    </div>
