d28b28d000f8efcc099c18b27fd2c84060957f6b
Waqas Khan <wuikhan@gmail.com>
2019-10-11 21:58:38 -0400

    menu added

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
         <header class="blog-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
                 <div class="col-12 text-center">
-                    <a class="blog-header-logo text-dark" href="#">Codegator Practice Site </a>
+                    <a class="blog-header-logo text-dark" href="Home.php">Codegator Practice Site </a>
                 </div>
             </div>
         </header>
