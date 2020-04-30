c5312e42826349855607014c60892d1daa99961e
wuikhan <wuikhan@gmail.com>
2020-04-30 09:47:16 -0400

    added multi select menu

--- bytag.php
+++ bytag.php
@@ -17,4 +17,11 @@
         </div>
     </div>
 
+    <select id="cars" multiple>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+    </select>
+
     <?php include('footer.php') ?>
