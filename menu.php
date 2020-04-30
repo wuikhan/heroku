81c9406bbca7c33b20514849806986747a917fff
wuikhan <wuikhan@gmail.com>
2020-04-30 11:40:54 -0400

    updated the navbar

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <div class="nav-scroller py-1 mb-2">
-  <nav class="nav d-flex justify-content-between">
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
     <a class="p-2 text-muted alertLink" href="alert.php">Alerts</a>
     <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
     <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
