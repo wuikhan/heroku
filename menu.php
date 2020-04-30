d2dbc7c055e17989055d6e52af5a30c9e3cb898b
wuikhan <wuikhan@gmail.com>
2020-04-30 12:09:55 -0400

    navbar fixed

--- menu.php
+++ menu.php
@@ -1,20 +1,38 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="index.php">codegator</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="p-2 text-muted alertLink" href="alert.php">Alerts</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
       </li>
+
+      <li class="nav-item">
+      <a class="p-2 text-muted" href="input_fields.php">Input Fields</a>
+      </li>
+
+      <li class="nav-item">
+      <a class="p-2 text-muted" href="tables.php">Tables</a>
+      </li>
+
+      <li class="nav-item">
+      <a class="p-2 text-muted" href="drop_down.php">Drop Down</a>
+      </li>
+
+      <li class="nav-item">
+      <a class="p-2 text-muted" href="form.php">Form</a>
+      </li>
+
+
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
@@ -30,15 +48,3 @@
     </form>
   </div>
 </nav>
-
-<div class="nav-scroller py-1 mb-2">
-  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-    <a class="p-2 text-muted alertLink" href="alert.php">Alerts</a>
-    <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
-    <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
-    <a class="p-2 text-muted" href="input_fields.php">Input Fields</a>
-    <a class="p-2 text-muted" href="tables.php">Tables</a>
-    <a class="p-2 text-muted" href="drop_down.php">Drop Down</a>
-    <a class="p-2 text-muted" href="form.php">Form</a>
-  </nav>
-</div>
