a609392d6ce38124594c27097108a9fa07030422
wuikhan <wuikhan@gmail.com>
2020-04-30 10:37:40 -0400

    more changes

--- MultipleWindows.php
+++ MultipleWindows.php
@@ -1,7 +1,12 @@
 <?php include('header.php') ?>
 <?php include('menu.php') ?>
-<hr>
-<h1> Multiple Windows </h1>
-
-<a href="https://www.google.com/" target="_blank" id="loginLink">login page</a>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h1> Multiple Windows </h1>
+            <p> Below link will open facebook in a new tab </p>
+            <a href="https://www.facebook.com/" target="_blank" id="facebookLink">facebook page</a>
+        </div>
+    </div>
+</div>
 <?php include('footer.php') ?>
--- menu.php
+++ menu.php
@@ -1,6 +1,5 @@
 <div class="nav-scroller py-1 mb-2">
   <nav class="nav d-flex justify-content-between">
-  <a class="p-2 text-muted alertLink" href="Home.php">Home</a>
     <a class="p-2 text-muted alertLink" href="index.php">Alerts</a>
     <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
     <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
