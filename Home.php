0fe3de11ea0a4d7ae02ddf58a119a9bda27dc313
wuikhan <wuikhan@gmail.com>
2020-04-30 11:09:43 -0400

    updates to the header

--- Home.php
+++
@@ -1,6 +0,0 @@
-<?php include('header.php'); ?>
-<?php include('menu.php'); ?>
-
-<h1> Practice Element Locators </h1>
-<button type="button" class="btn btn-primary" id="button">Primary Button</button>
-<?php include('footer.php'); ?>
