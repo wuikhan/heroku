6b438cea69732234405a00cfb2311f6db5ec34a8
wuikhan <wuikhan@gmail.com>
2020-04-30 10:52:03 -0400

    index.php added

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <div class="nav-scroller py-1 mb-2">
   <nav class="nav d-flex justify-content-between">
-    <a class="p-2 text-muted alertLink" href="alert.php">Alerts</a>
+    <a class="p-2 text-muted alertLink" href="index.php">Alerts</a>
     <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
     <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
     <a class="p-2 text-muted" href="input_fields.php">login</a>
