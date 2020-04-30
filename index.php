780f6eae4ffceba2ab48dfd9f3e80c5046d0f882
wuikhan <wuikhan@gmail.com>
2020-04-30 10:28:58 -0400

    made changes to the index page

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php include('header.php') ?>
 <?php include('menu.php') ?>
-<hr>
-<h1> Alert Practice </h1>
-<p>Below button is an example of simple alert button.</p>
-<button type="button" class="btn btn-outline-primary" onclick="myFunction()" id="simpleBtn">Simple Alert</button>
-<hr>
-<p>Click the button to display a confirm box.</p>
-<button type="button" class="btn btn-outline-primary" onclick="confirmAlertBtn()" id="confirmBtn">Confirm
-  Alert</button>
-<hr>
-<p>Click the button to demonstrate the prompt box.</p>
-<button type="button" class="btn btn-outline-primary" onclick="inputBtn()" id="promptBtn">Prompt Alert</button>
-<p id="demo"></p>
-
-<button type="button" class="btn btn-lg btn-primary" disabled>Primary button</button>
-<button type="button" class="btn btn-secondary btn-lg" disabled>Button</button>
-<hr>
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <h1> Alert Practice </h1>
+      <p>Below button is an example of simple alert button.</p>
+      <button type="button" class="btn btn-outline-primary" onclick="myFunction()" id="simpleBtn">Simple Alert</button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      <p>Click the button to display a confirm box.</p>
+      <button type="button" class="btn btn-outline-primary" onclick="confirmAlertBtn()" id="confirmBtn">Confirm Alert</button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      <p>Click the button to demonstrate the prompt box.</p>
+      <button type="button" class="btn btn-outline-primary" onclick="inputBtn()" id="promptBtn">Prompt Alert</button>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      <p id="demo">Below is an example for the disable button.</p>
+      <button type="button" class="btn btn-lg btn-primary" disabled>Primary button</button>
+      <button type="button" class="btn btn-secondary btn-lg" disabled>Disable Button</button>
+    </div>
+  </div>
+</div>
 <?php include('footer.php') ?>
