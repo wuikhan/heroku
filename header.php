81c5a0403bf0e1576369a3e52a2c7b23c58b0183
wuikhan <wuikhan@gmail.com>
2020-04-30 10:20:53 -0400

    updated the css stylesheet

--- header.php
+++ header.php
@@ -12,7 +12,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
-    <link href="blog.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/iwa1uky.css">
+    <link href="style.css" rel="stylesheet">
 </head>
 
 <body>
