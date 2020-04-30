1e3a971c4ba834b5a192938fc92f69b8aa29ec14
wuikhan <wuikhan@gmail.com>
2020-04-30 12:15:33 -0400

    removed toggle

--- menu.php
+++ menu.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
   <a class="navbar-brand" href="index.php">codegator</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
