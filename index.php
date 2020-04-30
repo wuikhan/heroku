637947977c69f02a8eace39043443e96a74504cf
wuikhan <wuikhan@gmail.com>
2020-04-30 12:58:31 -0400

    home page

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
   <h1 class="display-4">Hello, Students!</h1>
   <p class="lead">This is a dummy application, where you can practice by working on all the elements</p>
   <hr class="my-4">
-  <p>Report any issues to <a> info@codegator.us</a>.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <p>Report any issues to <a href="#">info@codegator.us</a></p>
 </div>
 
 <?php include('footer.php') ?>
