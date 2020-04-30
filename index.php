a4c128d59adfffafa85b586d3d48ee1ba6a6e4a1
wuikhan <wuikhan@gmail.com>
2020-04-30 12:44:52 -0400

    some changes to the index page

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php include('header.php') ?>
 <?php include('menu.php') ?>
 
-<h1>homepage</h1>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, Students!</h1>
+  <p class="lead">This is a dummy application, where you can practice by working on all the elements</p>
+  <hr class="my-4">
+  <p>Report any issues to <a> info@codegator.us</a>.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
 <?php include('footer.php') ?>
