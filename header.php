b85cd10b92c0b6da1461ec1071eec3baa074279c
wuikhan <wuikhan@gmail.com>
2020-04-30 10:47:19 -0400

    more...

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Custom styles for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/iwa1uky.css">
     <link href="style.css" rel="stylesheet">
 </head>
