aef0222418fcc6b3ed3ef99b7e3353fa809c797d
wuikhan <wuikhan@gmail.com>
2020-04-30 12:23:19 -0400

    nav fixed

--- menu.php
+++ menu.php
@@ -4,13 +4,15 @@
   <div class="container d-flex flex-column flex-md-row justify-content-between">
   <a class="navbar-brand" href="index.php">codegator</a>
     <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+    <a class="p-2 text-muted alertLink" href="alert.php">Alerts</a>
+    <a class="p-2 text-muted" id="iframe" href="iframe.php">Iframes</a>
+    <a class="p-2 text-muted" id="multipleWindow" href="MultipleWindows.php">Practice Multiple Windows</a>
+    <a class="p-2 text-muted" href="input_fields.php">Input Fields</a>
+    <a class="p-2 text-muted" href="tables.php">Tables</a>
+    <a class="p-2 text-muted" href="drop_down.php">Drop Down</a>
+    <a class="p-2 text-muted" href="form.php">Form</a>
   </div>
 </nav>
 
 <div class="container">
+
