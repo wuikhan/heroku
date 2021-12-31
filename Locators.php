4e15b913099483e37c653736b00d985c49b9ac49
Waqas Khan <me@waqaskhan.dev>
2021-12-31 16:04:02 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -7,10 +7,11 @@
 <p id="line1"> in this example we are learning css selector using tag & id </p>
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
 <br>
+<hr>
 <h2>if you have class attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h2>
 <p class="example line2"> in this example we are learning css selector using tag & class name </p>
-<code>driver.findElement(By.cssSelector(“p.example”));</code>
-<code>driver.findElement(By.cssSelector(“p.example.line2”));</code>
+<code>driver.findElement(By.cssSelector(“p.example”));</code><br>
+<code>driver.findElement(By.cssSelector(“p.example.line2”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.line2));</code>
 <br>
 
