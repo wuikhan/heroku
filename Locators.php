416f4ee78b43954a2cc558b23b188906b008ad32
Waqas Khan <me@waqaskhan.dev>
2021-12-31 16:13:57 -0500

    updates

--- Locators.php
+++ Locators.php
@@ -2,13 +2,13 @@
 <?php include('menu.php') ?>
 
 
-<h1>CSS Selector Example </p>
-<h2>if you have id attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h2>
+<h5>CSS Selector Example </h5>
+<h6>if you have id attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6>
 <p id="line1"> in this example we are learning css selector using tag & id </p>
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
 <br>
 <hr>
-<h2>if you have class attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h2>
+<h6>if you have class attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h6>
 <p class="example line2"> in this example we are learning css selector using tag & class name </p>
 <code>driver.findElement(By.cssSelector(“p.example”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.example.line2”));</code><br>
