f3516b6971fba5d9b543d7f947828d8e3119ea31
Waqas Khan <me@waqaskhan.dev>
2021-12-31 15:55:00 -0500

    some code for css selector

--- Locators.php
+++ Locators.php
@@ -2,9 +2,11 @@
 <?php include('menu.php') ?>
 
 
-<p?>Hello</p>
+<h1>CSS Selector Example </p>
+<h2>Using "tag#id" </h2>
+<p id="line1"> in this example we are learning css selector </p>
 
-
+<code>driver.findElement(By.cssSelector(“button#buttonId”)).click();</code>
 
 
 <?php include('footer.php') ?>
