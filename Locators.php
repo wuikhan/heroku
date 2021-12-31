97366a1c4477901aaa5655ee880524418ab65d2b
Waqas Khan <me@waqaskhan.dev>
2021-12-31 16:29:22 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -5,14 +5,14 @@
 <h5>CSS Selector Example </h5>
 <b><i>tag#id</i></b>
 <h6>if you have id attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6>
-<p id="line1" STYLE="text-decoration:blink"> Capture this text and print it in the console. (using tag#id) </p>
+<p id="line1" style="text-decoration:blink"> Capture this text and print it in the console. (using tag#id) </p>
 
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
 <br>
 <hr>
 <b><i>tag.class</i></b>
 <h6>if you have class attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h6>
-<p class="example line2" STYLE="text-decoration:blink"> Capture this text and print it in the console. (using tag.class) </p>
+<p class="example line2" style="text-decoration:blink"> Capture this text and print it in the console. (using tag.class) </p>
 <code>driver.findElement(By.cssSelector(“p.example”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.example.line2”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.line2));</code>
