df3013173df37bfc4dd194eb4b3a637d777a146a
Waqas Khan <me@waqaskhan.dev>
2021-12-31 16:16:01 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -3,8 +3,11 @@
 
 
 <h5>CSS Selector Example </h5>
+tag#id
 <h6>if you have id attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6>
 <p id="line1"> in this example we are learning css selector using tag & id </p>
+
+<code> <p id="line1"> some text ... </p>
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
 <br>
 <hr>
