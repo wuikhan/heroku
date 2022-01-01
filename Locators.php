b4db781d4c93f6e774845b7a0e95ba488ca0df00
Waqas Khan <me@waqaskhan.dev>
2022-01-01 12:59:00 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -12,7 +12,7 @@
 <code>driver.findElement(By.cssSelector(“p.example”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.example.line2”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.line2));</code>
-<br>
+<hr>
 <b><i>tag[attribute=’attribute value’]</i></b>
 <h6>if you dont have any <code>id</code> or <code>class</code> present in the webelement, you can create custom cssSelector</h6>
 <code>driver.findElement(By.cssSelector(“input[placeholder='First name']”));</code>
