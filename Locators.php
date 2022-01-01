e86d7648e53551d9010ac87efc502df933e74be7
Waqas Khan <me@waqaskhan.dev>
2022-01-01 12:47:30 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -2,7 +2,7 @@
 <?php include('menu.php') ?>
 
 
-<h5>CSS Selector Example </h5>
+<h5><u>Using CSS Selector </u></h5>
 <b><i>tag#id</i></b>
 <h6>if you have <code>id</code> attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6>
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
@@ -15,6 +15,6 @@
 <br>
 <hr>
 
-
+<h5><u>Using XPATH </u></h5>
 
 <?php include('footer.php') ?>
