24f596a93959259587b2e25548c817e224f607fe
Waqas Khan <me@waqaskhan.dev>
2022-01-01 12:44:36 -0500

    Update Locators.php

--- Locators.php
+++ Locators.php
@@ -4,15 +4,12 @@
 
 <h5>CSS Selector Example </h5>
 <b><i>tag#id</i></b>
-<h6>if you have id attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6><br>
-<p id="line1"> (using tag#id) To test this out we will clear the text and enter new text in the below field using css selector.</p>
-<input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
+<h6>if you have <code>id</code> attribute present you can combine with the tag and id like e.g: tag#id and it will find that element.</h6><br>
 <code>driver.findElement(By.cssSelector(“p#line1”));</code>
 <br>
 <hr>
 <b><i>tag.class</i></b>
-<h6>if you have class attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h6>
-<p class="example line2" style="text-decoration:blink"> Capture this text and print it in the console. (using tag.class) </p>
+<h6>if you have <code>class</code> attribute present you can combine with the tag and class like e.g: tag.class and it will find that element.</h6>
 <code>driver.findElement(By.cssSelector(“p.example”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.example.line2”));</code><br>
 <code>driver.findElement(By.cssSelector(“p.line2));</code>
