fdb2cbb27b6b60334e22aa7a84d16c4897b8cd95
Waqas Khan <me@waqaskhan.dev>
2023-05-04 13:37:44 -0400

    updated

--- iframe.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php') ?>
-<?php include('menu.php') ?>
-
-<h1> Iframe Practice </h1>
-<iframe id="frame1" src="input_fields.php"></iframe>
-<iframe id="frame2" src="input_fields.php"></iframe>
-<?php include('footer.php') ?>
